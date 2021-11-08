dabf100ffaabdab4907ad7c3105903c651d62dea
marijanagardovic <marijana.gardovic99@gmail.com>
2021-11-08 13:33:27 +0100

    added function show/hide for cart container

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const toggleBtn = document.querySelector('.navbar-toggle');
 const collapse = document.querySelector('.navbar-collapse');
+const cartBtn = document.querySelector('#cart-btn');
+const cartContainer = document.querySelector('.cart-container');
 
 toggleBtn.addEventListener('click', () => {
     collapse.classList.toggle('show-navbar');
+})
+
+cartBtn.addEventListener('click', () => {
+    cartContainer.classList.toggle('show-cart-container');
 })

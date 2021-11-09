19a7615b1eb747f82c38adca893d919d2458afc3
marijanagardovic <marijana.gardovic99@gmail.com>
2021-11-09 11:02:08 +0100

    added function for loading JSON file

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const toggleBtn = document.querySelector('.navbar-toggle');
 const collapse = document.querySelector('.navbar-collapse');
 const cartBtn = document.querySelector('#cart-btn');
 const cartContainer = document.querySelector('.cart-container');
+const productList = document.querySelector('.product-list');
+
 
 toggleBtn.addEventListener('click', () => {
     collapse.classList.toggle('show-navbar');
@@ -10,3 +12,31 @@ toggleBtn.addEventListener('click', () => {
 cartBtn.addEventListener('click', () => {
     cartContainer.classList.toggle('show-cart-container');
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadJSON();
+})
+
+function loadJSON() {
+    fetch('products.json')
+    .then(response => response.json())
+    .then(products => {
+        let html = '';
+        products.forEach(product => {
+            html += `
+            <div class="product-item">
+                <div class="product-image">
+                    <img src="${product.imgSrc}" alt="product-image">
+                    <button type="button" class="add-to-cart"><i class="fas fa-shopping-cart"></i>Add to cart</button>
+                </div>
+                <div class="product-content">
+                    <h3 class="product-name">${product.name}</h3>
+                    <span class="product-category">${product.category}</span>
+                    <p class="product-price">$${product.price}</p>
+                </div>
+            </div>
+            `;
+        });
+        productList.innerHTML = html;
+    })
+}
